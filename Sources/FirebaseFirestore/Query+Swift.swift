10dcccad4c75f79862741caf587bf52955c47b20
Darin Fisher <darinf@gmail.com>
2024-02-12 16:19:26 -0800

    cleanup

--- Sources/FirebaseFirestore/Query+Swift.swift
+++ Sources/FirebaseFirestore/Query+Swift.swift
@@ -9,89 +9,17 @@ import CxxShim
 
 public typealias Query = firebase.firestore.Query
 
-/*
-extension FutureProtocol {
-  func wait(completion: @escaping (Bool) -> Void) {
-    FutureSupport.runWork({
-      Wait(firebase.FutureBase.kWaitTimeoutInfinite)
-    }, completion: completion)
-  }
-}
-*/
-
 extension Query {
   var firestore: Firestore {
     swift_firebase.swift_cxx_shims.firebase.firestore.query_firestore(self)
   }
-        /*
-        if let error = future.error {
-          completion(nil, error)
-        } else {
-          completion(future.result, nil)
-        }
-        */
 
   func getDocuments(completion: @escaping (QuerySnapshot?, Error?) -> Void) {
     let future = swift_firebase.swift_cxx_shims.firebase.firestore.query_get(self, .default)
     future.setCompletion({
-      let (result, error) = future.resultAndError
-      completion(result, error)
-
-      /*
-      if future.error() == 0 {
-        completion(future.__resultUnsafe().pointee, nil)
-      } else {
-        let message = String(cString: future.__error_messageUnsafe()!)
-        completion(nil, FirebaseError(code: future.error(), message: message))
-      }
-      */
+      let (snapshot, error) = future.resultAndError
+      completion(snapshot, error)
     })
-
-    /*
-    future.wait { result, error in
-      completion(result, error)
-    }
-    */
-
-    /*
-    future.wait { error in
-      if let error {
-        completion(nil, error)
-      } else {
-        completion(future.__resultUnsafe().pointee, nil)
-      }
-    }
-    */
-
-    #if false
-    future.wait(completion: { _ in
-      if future.error() == 0 {
-        completion(future.__resultUnsafe().pointee, nil)
-      } else {
-        let message = String(cString: future.__error_messageUnsafe()!)
-        completion(nil, FirebaseError(code: future.error(), message: message))
-      }
-
-      /*
-      typealias CompletionType = (QuerySnapshot?, Error?) -> Void
-      withUnsafePointer(to: completion) { completion in
-        swift_firebase.swift_cxx_shims.firebase.future_with_result_or_error(future, { result, error, pvCompletion in
-          let pCompletion = pvCompletion?.assumingMemoryBound(to: CompletionType.self)
-          if let result {
-            pCompletion.pointee(result, nil)
-          } else {
-            pCompletion.pointee(nil, FirebaseError(code: error, message: nil))
-          }
-        })
-      }
-      */
-      /*
-      future.withResultOrError { result, error in
-        completion(result, error)
-      }
-      */
-    })
-    #endif
   }
 
   func getDocuments() async throws -> QuerySnapshot {
