8a02699b30f72150baf2f6e76e888403b4586475
Darin Fisher <darinf@gmail.com>
2024-02-13 12:49:08 -0800

    Implement Query.addSnapshotListener

--- Sources/firebase/include/FirebaseFirestore.hh
+++ Sources/firebase/include/FirebaseFirestore.hh
@@ -119,19 +119,19 @@ collection_document(::firebase::firestore::CollectionReference collection,
   return collection.Document(document_path);
 }
 
-typedef void (*SnapshotListenerTypedCallback_SwiftWorkaround)(
+typedef void (*DocumentSnapshotListenerTypedCallback)(
     const ::firebase::firestore::DocumentSnapshot *snapshot,
     ::firebase::firestore::Error error_code, const char *error_message,
     void *user_data);
 inline ::firebase::firestore::ListenerRegistration
 document_add_snapshot_listener(
     ::firebase::firestore::DocumentReference document,
-    SnapshotListenerTypedCallback_SwiftWorkaround callback, void *user_data) {
+    DocumentSnapshotListenerTypedCallback callback, void *user_data) {
   return document.AddSnapshotListener(
-      [callback,
-       user_data](const ::firebase::firestore::DocumentSnapshot &snapshot,
-                  ::firebase::firestore::Error error_code,
-                  const std::string &error_message) {
+      [callback, user_data](
+          const ::firebase::firestore::DocumentSnapshot &snapshot,
+          ::firebase::firestore::Error error_code,
+          const std::string &error_message) {
         callback(&snapshot, error_code, error_message.c_str(), user_data);
       });
 }
@@ -182,6 +182,23 @@ query_get(const ::firebase::firestore::Query query,
              ::firebase::firestore::Source source =
                  ::firebase::firestore::Source::kDefault) {
   return query.Get(source);
+}
+
+typedef void (*QuerySnapshotListenerTypedCallback)(
+    const ::firebase::firestore::QuerySnapshot *snapshot,
+    ::firebase::firestore::Error error_code, const char *error_message,
+    void *user_data);
+inline ::firebase::firestore::ListenerRegistration
+query_add_snapshot_listener(
+    ::firebase::firestore::Query query,
+    QuerySnapshotListenerTypedCallback callback, void *user_data) {
+  return query.AddSnapshotListener(
+      [callback, user_data](
+          const ::firebase::firestore::QuerySnapshot &snapshot,
+          ::firebase::firestore::Error error_code,
+          const std::string &error_message) {
+        callback(&snapshot, error_code, error_message.c_str(), user_data);
+      });
 }
 
 } // namespace swift_firebase::swift_cxx_shims::firebase::firestore
