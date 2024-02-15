848da384b853c633c27508313d15103549c0fb07
Darin Fisher <darinf@gmail.com>
2024-02-14 16:26:26 -0800

    cleanup

--- Sources/FirebaseFirestore/DocumentReference+Swift.swift
+++ Sources/FirebaseFirestore/DocumentReference+Swift.swift
@@ -64,24 +64,26 @@ extension DocumentReference {
   public func addSnapshotListener(includeMetadataChanges: Bool, listener: @escaping (DocumentSnapshot?, Error?) -> Void) -> ListenerRegistration {
     typealias ListenerCallback = (DocumentSnapshot?, Error?) -> Void
     let boxed = Unmanaged.passRetained(listener as AnyObject)
-    let instance = swift_firebase.swift_cxx_shims.firebase.firestore.document_add_snapshot_listener(self, { snapshot, errorCode, errorMessage, pvListener in
-      let callback = Unmanaged<AnyObject>.fromOpaque(pvListener!).takeUnretainedValue() as! ListenerCallback
+    let instance = swift_firebase.swift_cxx_shims.firebase.firestore.document_add_snapshot_listener(
+      self, { snapshot, errorCode, errorMessage, pvListener in
+        let callback = Unmanaged<AnyObject>.fromOpaque(pvListener!).takeUnretainedValue() as! ListenerCallback
 
-      let error = NSError.firestore(errorCode, errorMessage: errorMessage)
-      // We only return a snapshot if the error code isn't 0 (aka the 'ok' error code)
-      let returned = error == nil ? snapshot?.pointee : nil
+        let error = NSError.firestore(errorCode, errorMessage: errorMessage)
+        // We only return a snapshot if the error code isn't 0 (aka the 'ok' error code)
+        let returned = error == nil ? snapshot?.pointee : nil
 
-      // Make sure we dispatch our callback back into the main thread to keep consistent
-      // with the reference API which will call back on the 'user_executor' which typically
-      // ends up being the main queue.
-      // Relevant code:
-      // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/Source/API/FIRFirestore.mm#L210-L218
-      // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/core/src/api/document_reference.cc#L236-L237
-      DispatchQueue.main.async {
-        callback(returned, error)
-      }
-    }, UnsafeMutableRawPointer(boxed.toOpaque()))
-
+        // Make sure we dispatch our callback back into the main thread to keep consistent
+        // with the reference API which will call back on the 'user_executor' which typically
+        // ends up being the main queue.
+        // Relevant code:
+        // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/Source/API/FIRFirestore.mm#L210-L218
+        // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/core/src/api/document_reference.cc#L236-L237
+        DispatchQueue.main.async {
+          callback(returned, error)
+        }
+      },
+      boxed.toOpaque()
+    )
     return ListenerRegistration(boxed, instance)
   }
 
