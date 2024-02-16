3cff95239f8f8773acdce34be635a1af83e383f1
Darin Fisher <darinf@gmail.com>
2024-02-15 22:25:15 -0800

    add @discardableResult to Transaction methods

--- Sources/FirebaseFirestore/Transaction+Swift.swift
+++ Sources/FirebaseFirestore/Transaction+Swift.swift
@@ -9,28 +9,28 @@ import Foundation
 public typealias Transaction = swift_firebase.swift_cxx_shims.firebase.firestore.TransactionWeakReference
 
 extension Transaction {
-  public func setData(_ data: [String : Any], forDocument document: DocumentReference) -> Transaction {
+  public @discardableResult func setData(_ data: [String : Any], forDocument document: DocumentReference) -> Transaction {
     setData(data, forDocument: document, merge: false)
   }
 
-  public func setData(_ data: [String : Any], forDocument document: DocumentReference, merge: Bool) -> Transaction {
+  public @discardableResult func setData(_ data: [String : Any], forDocument document: DocumentReference, merge: Bool) -> Transaction {
     guard is_valid() else { fatalError("Transaction accessed outside of updateBlock") }
     self.Set(document, FirestoreDataConverter.firestoreValue(document: data), merge ? .Merge() : .init())
     return self
   }
 
   /* TODO: implement
-  public func setData(_ data: [String : Any], forDocument document: DocumentReference, mergeFields: [Any]) -> Transaction {
+  public @discardableResult func setData(_ data: [String : Any], forDocument document: DocumentReference, mergeFields: [Any]) -> Transaction {
   }
   */
 
-  public func updateData(_ fields: [String : Any], forDocument document: DocumentReference) -> Transaction {
+  public @discardableResult func updateData(_ fields: [String : Any], forDocument document: DocumentReference) -> Transaction {
     guard is_valid() else { fatalError("Transaction accessed outside of updateBlock") }
     self.Update(document, FirestoreDataConverter.firestoreValue(document: fields))
     return self
   }
 
-  public func deleteDocument(_ document: DocumentReference) -> Transaction {
+  public @discardableResult func deleteDocument(_ document: DocumentReference) -> Transaction {
     guard is_valid() else { fatalError("Transaction accessed outside of updateBlock") }
     Delete(document)
     return self
