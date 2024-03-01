250f8f5f470feeb3efc3d81f845ebefe6df8d4b9
Darin Fisher <darinf@gmail.com>
2024-03-01 13:03:20 -0800

    Implement FirestoreErrorCode

---
+++ Sources/FirebaseFirestore/FirestoreErrorCode.swift
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+public final class FirestoreErrorCode: Error {
+  public typealias Code = firebase.firestore.Error
+
+  public let code: Code
+  public let localizedDescription: String
+
+  public init(_ error: firebase.firestore.Error, errorMessage: String?) {
+    code = error
+    localizedDescription = errorMessage ?? "\(code.rawValue)"
+  }
+
+  public convenience init?(_ error: firebase.firestore.Error?, errorMessage: UnsafePointer<CChar>? = nil) {
+    guard let actualError = error, actualError.rawValue != 0 else { return nil }
+    var errorMessageString: String?
+    if let errorMessage {
+      errorMessageString = .init(cString: errorMessage)
+    }
+    self.init(actualError, errorMessage: errorMessageString)
+  }
+}
