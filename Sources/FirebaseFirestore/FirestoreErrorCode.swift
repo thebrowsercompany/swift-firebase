6d650cc27185d8b1379cd57c81ac16c0b78043f8
Darin Fisher <darinf@gmail.com>
2024-03-04 09:34:50 -0800

    add error code wrappers

--- Sources/FirebaseFirestore/FirestoreErrorCode.swift
+++ Sources/FirebaseFirestore/FirestoreErrorCode.swift
@@ -22,3 +22,26 @@ public struct FirestoreErrorCode: Error {
 }
 
 extension FirestoreErrorCode: Equatable {}
+
+// Unfortunately `Error` is not defined as a `enum class` so we need to add
+// these wrappers.
+extension FirestoreErrorCode.Code {
+  static var ok: Self { firebase.firestore.kErrorOk }
+  static var none: Self { firebase.firestore.kErrorNone }
+  static var cancelled: Self { firebase.firestore.kErrorCancelled }
+  static var unknown: Self { firebase.firestore.kErrorUnknown }
+  static var invalidArgument: Self { firebase.firestore.kErrorInvalidArgument }
+  static var deadlineExceeded: Self { firebase.firestore.kErrorDeadlineExceeded }
+  static var notFound: Self { firebase.firestore.kErrorNotFound }
+  static var alreadyExists: Self { firebase.firestore.kErrorAlreadyExists }
+  static var permissionDenied: Self { firebase.firestore.kErrorPermissionDenied }
+  static var resourceExhausted: Self { firebase.firestore.kErrorResourceExhausted }
+  static var failedPrecondition: Self { firebase.firestore.kErrorFailedPrecondition }
+  static var aborted: Self { firebase.firestore.kErrorAborted }
+  static var outOfRange: Self { firebase.firestore.kErrorOutOfRange }
+  static var unimplemented: Self { firebase.firestore.kErrorUnimplemented }
+  static var `internal`: Self { firebase.firestore.kErrorInternal }
+  static var unavailable: Self { firebase.firestore.kErrorUnavailable }
+  static var dataLoss: Self { firebase.firestore.kErrorDataLoss }
+  static var unauthenticated: Self { firebase.firestore.kErrorUnauthenticated }
+}
